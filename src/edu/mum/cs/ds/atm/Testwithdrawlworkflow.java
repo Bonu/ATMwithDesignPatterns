170d807035c294f21f9e53eb59dcddf38835440c
sgummadidala <cnu.gsrinivas@gmail.com>
2015-07-14 13:55:43 -0500

    commited

---
+++ src/edu/mum/cs/ds/atm/Testwithdrawlworkflow.java
@@ -0,0 +1,21 @@
+package edu.mum.cs.ds.atm;
+
+import edu.mum.cs.ds.atm.model.ATMMachine;
+
+public class Testwithdrawlworkflow {
+	
+	/*
+	 * 1. Check the state of the ATM Machine, if it is NotEnoughCash state we should not proceed.
+	 * 2. Check the state of the account, if it is red we should not proceed
+	 * 3. Issue withdrawl command
+	 * 4. Send SMS to user Mobile
+	 * 5. Log the transaction for future audit
+	 * 6. Print Reciept
+	*/
+
+	ATMMachine objMachine = new ATMMachine();
+	
+	
+	
+	
+}
--- src/edu/mum/cs/ds/atm/model/ATMMachine.java
+++ src/edu/mum/cs/ds/atm/model/ATMMachine.java
@@ -3,5 +3,7 @@ package edu.mum.cs.ds.atm.model;
 import java.io.Serializable;
 
 public class ATMMachine implements Serializable {
+	public String location;
+	public String Id;
 
 }
