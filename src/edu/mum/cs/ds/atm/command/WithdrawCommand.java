053c079b45995925e3ada73c338d251c20db4958
janardhanbonu <janardhanbonu@gmail.com>
2015-07-14 21:32:29 -0500

    -u

--- src/edu/mum/cs/ds/atm/command/WithdrawCommand.java
+++ src/edu/mum/cs/ds/atm/command/WithdrawCommand.java
@@ -7,20 +7,19 @@ import edu.mum.cs.ds.atm.model.Request;
 public class WithdrawCommand implements UndoCommand {
 
 	private Request request;
-	private Reciever objReciever;
-	
-	public WithdrawCommand(Request request, Reciever reciever){
+	private Reciever receiver;
+	public WithdrawCommand(Request request){
 		this.request=request;
-		this.objReciever = reciever;
+		receiver = new Reciever();
 	}
 	@Override
 	public boolean execute() {
-		return objReciever.withdrawl(request);
+		return receiver.withdrawl(request);
 	}
 
 	@Override
 	public boolean undo() {
-		 return objReciever.undowithdrawl(request);
+		 return receiver.undowithdrawl(request);
 	}
 
 }
