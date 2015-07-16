804060696c2d36a663978ff6a4fcd27f8a87eefc
janardhanbonu <janardhanbonu@gmail.com>
2015-07-16 00:20:44 -0500

    -

--- src/edu/mum/cs/ds/atm/base/SingletonSMS.java
+++ src/edu/mum/cs/ds/atm/base/SingletonSMS.java
@@ -1,14 +1,8 @@
 package edu.mum.cs.ds.atm.base;
 
-import java.text.SimpleDateFormat;
-import java.util.Date;
-import java.util.logging.FileHandler;
-import java.util.logging.Level;
 import java.util.logging.Logger;
-import java.util.logging.SimpleFormatter;
 
 import edu.mum.cs.ds.atm.SMSFacade;
-import edu.mum.cs.ds.atm.model.Response;
 
 public class SingletonSMS {
 	public static final Logger myLogger = Logger.getLogger("Test");
@@ -19,25 +13,6 @@ public class SingletonSMS {
 			instance = new SingletonSMS();
 		}
 		return instance;
-	}
-
-	public static void sendSMS(Response response){
-		String accountId = response.getRequest().getAccountId();
-	}
-	
-	private static void prepareLogger() {
-		Date date = new Date();
-		SimpleDateFormat sdf = new SimpleDateFormat("MMddYYYYHHmm");
-		String sDate = sdf.format(date);
-		try {
-			FileHandler myFileHandler = new FileHandler("myLogFile"+sDate+".log", true);
-			myFileHandler.setFormatter(new SimpleFormatter());
-			myLogger.addHandler(myFileHandler);
-			myLogger.setUseParentHandlers(false);
-			myLogger.setLevel(Level.ALL);
-		} catch (Exception e) {
-			// ...
-		}
 	}
 	
 	public void setMessage(String phoneNumber, String message){
